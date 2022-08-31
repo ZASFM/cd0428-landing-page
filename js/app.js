b9c50daf3674138d902ad1e678b575f496433c00
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-08-31 12:22:30 +0200

    Scrolling done, smooth scrolling left

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@
  * 
 */
 
-// build the nav
+// build the nav, here im building a li element and nesting also anchor tags inside then:
 let sectionCount=document.querySelectorAll('section');
 let navbar__list=document.querySelector('#navbar__list');
 let liCounter=0;
@@ -48,20 +48,21 @@ for(let i=0;i<=sectionCount.length-1;i++){
    liCounter++;
    let anchors=document.createElement('a');
    anchors.href=`#section${liCounter}`;
+   anchors.classList.add('smoothTransition')
    anchors.innerText=`Section ${liCounter}`;
    li.appendChild(anchors);
    navbar__list.appendChild(li);
 }
 
-// Add class 'active' to section when near top of viewport
-
+// Add class 'active' to section when near top of viewport, this part keep track of which section the viewport is showing, 
+//enlightens the respective nav element to that section displayed:
+let anchor=document.querySelectorAll('a');
 function makeActive(){
    for (let section of sectionCount) {
        const box = section.getBoundingClientRect();
        const value=150;
        if (box.top <= value && box.bottom >= value) {
           section.classList.add('active');
-          let anchor=document.querySelectorAll('a');
             for(let i=0;i<anchor.length;i++){
                if(anchor[i].innerText.toString()===section.getAttribute('data-nav').toString()){
                   anchor[i].parentElement.classList.add('selected');
@@ -76,54 +77,35 @@ function makeActive(){
        }
     }
  }
- document.addEventListener("scroll", function() { makeActive();});
+document.addEventListener("scroll", function() { makeActive();});
+
+//This part takes care of the scrolling to the correspondent section when one of the nav element are clicked using a click event:
+for(let i=0;i<anchor.length;i++){
+   anchor[i].addEventListener('click',function(event){
+      event.preventDefault();
+      let target=event.target;
+      for(let j=0;j<sectionCount.length;j++){
+         if(target.getAttribute('href').slice(1,this.length)===sectionCount[j].id){
+            location.href=`#section${sectionCount[j].id.slice(7,9)}`;
+         }
+      }
+   })
+}
+
+document.querySelector('.navbar__menu').addEventListener('click', function (e) {
+   /* e.preventDefault(); */
+   const target = e.target;
+   if (target.classList.contains('smoothTransition')) {
+       const id = target.getAttribute('href').slice(1);
+       document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+   }
+});
 
  let liEl=document.getElementsByClassName('menu__link');
  let ulEl=document.getElementById('navbar__list');
- ulEl.addEventListener('mouseover',function(event){
-   let target=event.target;
-   if(target.matches('a')){
-      target.setAttribute('style','color:white;')
-   }
- })
 
- ulEl.addEventListener('mouseout',function(event){
-   let target=event.target;
-   if(target.matches('a')){
-      target.setAttribute('style','background-color:none;')
-   }
- })
- for(let li of liEl){
-   li.addEventListener('mouseover',function(e){
-      let target=e.target;
-      target.style.color='white';
-   })
- }
-
- for(let li of liEl){
-   li.addEventListener('mouseout',function(e){
-      let target=e.target;
-      if(!target.classList.contains('selected') && target.matches('a')){
-         console.log(true);
-      }
-      target.style.color='black';
-   })
- }
-/*  document.addEventListener('scroll',function checkActive(){
-   
- }) */
 // Scroll to anchor ID using scrollTO event
-/* document.querySelector('.page__header').addEventListener('click',function(event){
-   let target=event.target;
-   let isActive=document.querySelector('.active');
-   if(isActive){
-      target.classList.remove('active')
-   }
-   if(target.classList.contains('menu__link')){
-      target.classList.add('active');
-   }
-})
- */
+
 /**
  * End Main Functions
  * Begin Events
@@ -135,18 +117,3 @@ function makeActive(){
 // Scroll to section on link click
 
 // Set sections as active
-
-//Successful message:
-/* const body=document.querySelector('body')
-const formButton=document.getElementById('button');
-formButton.addEventListener('click',function displayMessage(){
-   let outerDiv=document.createElement('div');
-   let innerDiv=document.createElement('div');
-   outerDiv.classList.add('succesfullContainer');
-   innerDiv.classList.add('succesfullMessage');
-   let text=document.createTextNode('Your submission was received, now close this window please');
-   innerDiv.appendChild(text);
-   outerDiv.appendChild(innerDiv);
-   body.appendChild(outerDiv);
-   console.log(innerDiv,outerDiv);
-}) */
