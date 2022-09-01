81fb247d5bd294782a273e68f0b9b0c5cadbfc4e
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-09-01 21:05:54 +0200

    Marks added

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
-# Landing Page Project
+# Landing Page Project 
 
 ## Table of Contents
-
+* [Project-title](#landing-page-project)
+* [Table-of-contents](#table-of-contents)
 * [Instructions](#instructions)
 
 ## Instructions
+This project has three essential file that have all the code.
+1. styles.css nested inside the ccs directory.
+2. app.js nested inside the js directory.
+3. index.html.
+This last file has all the structure and the content necessary for the page, and has its styling at the .css file.
+All the logics and the markups are present at the .js file.
+To be able to see this project it is necessary to have an IDE, then follow the next steps
+1. Go to the code button and copy the link.
+2. Open you terminal and use that link to clone the page.
+3. You can use two methods to run the page
+   * Have a live server extension and run it that way.
+   * Hove over the index.html file, right click, copy path and finally paste it in a browser
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-/*---------------------------------------------------Nav bar creation virtual DOM---------------------------------------------------*/
-// build the nav, here im building a li element and nesting also anchor tags inside then:
+/*---------------------------------------------------Nav bar elements creation virtual DOM---------------------------------------------------*/
 let sectionCount=document.querySelectorAll('section');
 let navbar__list=document.querySelector('#navbar__list');
 //Here im setting a counter than goes from zero to the number of sections
@@ -42,16 +41,16 @@ function makeActive(){
                //So now, the section that is currently in the viewport i want to get its data-nav attribute, and 
                //see with which of the anchor tags elements before it matches.
                //Note: im using the data-nav, and the anchor innerText, because both have a space =>section+' '+.....
-               if(anchor[i].innerText.toString()===section.getAttribute('data-nav').toString()){
+               if(
+                  anchor[i].innerText.toString()===section.getAttribute('data-nav').toString()
+               ){
                   //Once the are matched, i want to give that anchor a selected class, this way i can use CSS, to mark it
                   anchor[i].parentElement.classList.add('selected');
-                  /* anchor[i].style.color='white'; */
                }else{
                   //And if that section the previously was in the viewport and has the selected class, now it is not, i want to 
                   //delete the selected class out of it and give it to the anchor tag whose section is currently at the viewport
                   //Yh, it works like a .toggle 
                   anchor[i].parentElement.classList.remove('selected');
-                  /* anchor[i].style.color='black'; */
                }
             }
        }else {
@@ -78,35 +77,34 @@ for(let i=0;i<arrayOfAnchors.length;i++){
       //the smooth transition 
       event.preventDefault();
       //Here im getting the current target href attribute and setting a smooth scroll to it
-      document.querySelector(this.getAttribute('href')).scrollIntoView({behavior:'smooth'})
+      document.querySelector(
+         this.getAttribute('href')).scrollIntoView({behavior:'smooth'}
+         )
    })
 }
 /*------------------------------------------------------------------------------------------------------------------------------------*/
-
-
-// Scroll to anchor ID using scrollTO event
-//This is commented, because I received a review that I interpreted as:
-//The nav elements should scroll the the correct section, not with the anchor tags, but with an click event, so did I:
-//This part takes care of the scrolling to the correspondent section when one of the nav element are clicked using a click event:
-//Looping through the anchor list
-/* for(let i=0;i<anchor.length;i++){
-   //Setting an eventlistener to every anchor
-   anchor[i].addEventListener('click',function(event){
-      //Setting the .preventDefault(), so now at clicking at the anchor tags they won´t take me to the section:
-      event.preventDefault();
-      //Setting my target:
-      let target=event.target;
-      //Inside this loop Im looping through my list of sections:
-      for(let j=0;j<sectionCount.length;j++){
-         //Since im gonna click at one of the anchors, I want to get that anchors href and get rid of the # using slice
-         //then I want to match it with the id of the section
-         if(target.getAttribute('href').slice(1,this.length)===sectionCount[j].id){
-            //If the matching thrives, I want using the location.href I want to go the the section that has the same
-            //Id of my targeted anchor, and agin Im suing slice because i only want to get the number (0-4)
-            location.href=`#section${sectionCount[j].id.slice(7,this.length)}`;
-         }
-      }
-   })
-} */
-//The code above that is commented, worked, but now it wont accept the smooth transition, so there is a conflict
-//And I decided not to include it, but here you can see some more of my DOM skills. 
+/*----------------------------------------Setting the submit button to disable until I get my inputs filed----------------------------*/
+let submitBtn=document.getElementById('button');
+let arrayOfInputs=document.querySelectorAll('input');
+//Looping through list of inputs:
+for(let i=0;i<arrayOfInputs.length-1;i++){
+   //for everytime I type something in my inputs, so this is easier to pull out than setting a timeInterval()
+   arrayOfInputs[i].oninput=()=>{
+      //If my inputs from index 0-4 have something as value, so they are not empty
+      if (!(arrayOfInputs[0].value == '') 
+      && !(arrayOfInputs[1].value == '') 
+      && !(arrayOfInputs[2].value == '') 
+      && !(arrayOfInputs[3].value == '') 
+      && !(arrayOfInputs[4].value == '')) {
+         //then I want ot erase my disabled attribute from my submit input
+         submitBtn.removeAttribute('disabled');
+     }
+   }
+}
+//And now since everytime I click at submit input, even-though if the inputs are empty the alert will pop
+//so now  disabling the submit input until the inputs get fulfilled, will make sure that the 
+//alert popup wont show. 
+submitBtn.addEventListener('click',function(){
+   alert('Your application was successfully received');
+})
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
